af98b1b0903f51c6c1f13620aa11ed1e0753e40a
Adir Burke on Laptop <adir@hanave.com>
2020-10-07 12:41:08 +1100

    Initial Commit

--- Package.swift
+++ Package.swift
@@ -4,15 +4,17 @@ import PackageDescription
 let package = Package(
     name: "grpc",
     platforms: [
-       .macOS(.v10_14)
+       .macOS(.v10_15)
     ],
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-beta")
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-beta"),
+         .package(url: "https://github.com/grpc/grpc-swift.git", from: "1.0.0-alpha.6"),
     ],
     targets: [
         .target(name: "App", dependencies: [
-            "Vapor"
+            "Vapor",
+            "GRPC",
         ]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App", "XCTVapor"])
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,10 +1,43 @@
 import Vapor
 
+import GRPC
+
 // configures your application
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    
 
+    var gRPCServerConfig = Server.Configuration(target: .hostAndPort("0.0.0.0", 9000), eventLoopGroup: PlatformSupport.makeEventLoopGroup(loopCount: 1), serviceProviders: [GreeterProvider()])
+    
+    let server = try Server.start(configuration: gRPCServerConfig).whenSuccess { address in
+        print("GRPC Server started on port  \(address.channel.localAddress!.port!)")
+    }
+    
     // register routes
+    
+    
     try routes(app)
 }
+
+
+/*
+ 
+ extension Request {
+     public convenience init(
+         application: Application, context: ServerCallContext) {
+         self.init(application: application,
+                   method: context.request.method,  // .POST
+                   url: URI(path: context.request.uri),
+                   version: context.request.version,  // HTTP2
+                   headers: context.request.headers,
+                   eventLoop: context.eventLoop)
+     }
+ }
+ // In your gRPC provider’s handler, call:
+ let request = Request(application: self.app, context: context)
+ // Then use request as you usually would...
+
+ 
+ 
+ */
